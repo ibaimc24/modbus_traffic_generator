574b43d94f97bcd1687f43611046f8ede5b9a8ed
Ibai <imarcos009@gmail.com>
2018-06-05 15:47:36 +0200

    Refactor

--- modbus_traffic.py
+++ modbus_traffic.py
@@ -5,12 +5,19 @@ from protocol.codes import *
 import argparse
 import time
 
+# Slave IP Address
 DESTINATION_IP = "192.168.1.22"
+
+# Slave port
 DESTINATION_PORT = 502
+
+# Socket reciver buffer size in Bytes
 BUFFER_SIZE = 560
+
+# Number of packets per burst
 N_PACKETS = 1000
 
-# in seconds
+# Delay between sended packets in seconds
 DELAY = 0.1340666407541925
 
 
@@ -197,24 +204,23 @@ def w8(tic, toc):
         time.sleep(DELAY-tdelta)
 
 
-def interleaved(cod_1, cod_2):
+def interleaved(codes):
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((DESTINATION_IP, DESTINATION_PORT))
-    message1 = Modbus(code(cod_1)).raw
-    message2 = Modbus(code(cod_2)).raw
+    messages = []
+    for i in codes:
+        messages.append(Modbus(code(i)).raw)
     print("Connected")
     try:
         for i in range(N_PACKETS):
-            s.sendto(message1, (DESTINATION_IP, DESTINATION_PORT))
-            tic = datetime.now()
-            s.recv(BUFFER_SIZE)
-            toc = datetime.now()
-            w8(tic, toc)
-            s.sendto(message2, (DESTINATION_IP, DESTINATION_PORT))
-            tic = datetime.now()
-            s.recv(BUFFER_SIZE)
-            toc = datetime.now()
-            w8(tic, toc)
+            for a in messages:
+                s.sendto(a, (DESTINATION_IP, DESTINATION_PORT))
+                tic = datetime.now()
+                data = s.recv(BUFFER_SIZE)
+                if not data:
+                    print("wait for response!!!!")
+                toc = datetime.now()
+                w8(tic, toc)
     finally:
         s.close()
     print("DONE")
@@ -228,14 +234,14 @@ if __name__ == "__main__":
     parser.add_argument("-d", "--delay", help="Specifies the delay between sended packets: ", type=float, default=0.15)
     parser.add_argument("-n", "--number", help="Specifies the number of packets per burst: ", type=int, default=1000)
     parser.add_argument("--code", help="Specifies Modbus function code in decimal of the burst: ", type=int,
-                        choices=[1, 2, 3, 4, 15], action="append")
+                        choices=[1, 2, 3, 4, 5, 6, 7, 8, 11, 12, 15, 16, 17], action="append")
     parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true", default=False)
 
     args = parser.parse_args()
     if args.option == 1 and args.code is None:
         parser.error("--option requires --code")
     if args.option == 4 and len(args.code) < 2:
-        parser.error("--option 4 requires --code [code_1] [code_2]")
+        parser.error("--option 4 requires --code CODE_1 --code CODE_2 [--code CODE_3] [--code CODE_N]")
 
     DESTINATION_IP = args.addr
     DESTINATION_PORT = args.port
@@ -253,7 +259,7 @@ if __name__ == "__main__":
         print("Sending one packet of each Modbus function code...")
         one_of_each()
     elif args.option is 4:
-        print("Starting Interleaved burst with function codes {} and {}".format(args.code[0], args.code[1]))
-        interleaved(cod_1=args.code[0], cod_2=args.code[1])
+        print("Starting Interleaved burst with function codes ", *args.code)
+        interleaved(args.code)
     else:
         raise Exception("OptionError: Invalid option.")
