f0e3f665ed02e4e1a65843345bfde4ba50c8aa88
Ibai <imarcos009@gmail.com>
2018-06-05 15:16:22 +0200

    Arguments, delay and number of packets updated.

--- modbus_traffic.py
+++ modbus_traffic.py
@@ -1,114 +1,144 @@
 import socket
 from protocol.pkt import Modbus
-import time
+from datetime import datetime
 from protocol.codes import *
 import argparse
+import time
 
 DESTINATION_IP = "192.168.1.22"
 DESTINATION_PORT = 502
 BUFFER_SIZE = 560
+N_PACKETS = 1000
+
+# in seconds
+DELAY = 0.1340666407541925
 
 
 def continuous(code_value):
+    # Establish TCP Connection
+    message = Modbus(code(code_value)).raw
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((DESTINATION_IP, DESTINATION_PORT))
+    print("Connected to slave")
     try:
-        # Establish TCP Connection
-        message = Modbus(code(code_value)).raw
-
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((DESTINATION_IP, DESTINATION_PORT))
-        print("Connected to slave")
-        for i in range(1000):
-            # Create Modbus packet
-            message = Modbus(WRITE_MULTIPLE_COILS_Q).raw
-            # Send
-            #print()
+        for i in range(N_PACKETS):
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            # Read response
-            data = s.recv(BUFFER_SIZE)
-            #print(data)
-            # Wait for next request packet
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
     finally:
         s.close()
+    print("DONE")
 
 
 def bursts_of_main():
+    # Establish TCP Connection
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((DESTINATION_IP, DESTINATION_PORT))
+    print("Connected")
     try:
-        # Establish TCP Connection
-        s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-        s.connect((DESTINATION_IP, DESTINATION_PORT))
-        print("Connected")
-        for i in range(100):
+        for i in range(N_PACKETS):
             # Create Modbus packet
             message = Modbus(READ_COILS_Q).raw
-            # Send
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            # Read response
+            tic = datetime.now()
             s.recv(BUFFER_SIZE)
-            # print(data.hex)
-            # Wait for next request packet
+            toc = datetime.now()
+            w8(tic, toc)
 
-        for i in range(100):
-            # Create Modbus packet
+        for i in range(N_PACKETS):
             message = Modbus(READ_DISCRETE_INPUTS_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-        for i in range(100):
+        for i in range(N_PACKETS):
             message = Modbus(READ_HOLDING_REGISTERS_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-        for i in range(100):
+        for i in range(N_PACKETS):
             message = Modbus(READ_INPUT_REGISTERS_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-        for i in range(100):
+        for i in range(N_PACKETS):
             message = Modbus(WRITE_SINGLE_COIL_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-        for i in range(100):
+        for i in range(N_PACKETS):
             message = Modbus(WRITE_SINGLE_REGISTER_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-        for i in range(100):
+        for i in range(N_PACKETS):
             message = Modbus(READ_EXCEPTION_STATUS_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-        for i in range(100):
+        for i in range(N_PACKETS):
             message = Modbus(DIAGNOSTICS_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-        for i in range(100):
+        for i in range(N_PACKETS):
             message = Modbus(GET_COMM_EVENT_COUNTER_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-        for i in range(100):
+        for i in range(N_PACKETS):
             message = Modbus(GET_COMM_EVENT_LOG_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-
-        for i in range(100):
+        for i in range(N_PACKETS):
             message = Modbus(WRITE_MULTIPLE_COILS_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-        for i in range(100):
+        for i in range(N_PACKETS):
             message = Modbus(WRITE_MULTIPLE_REGISTERS_Q).raw
             s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-            data = s.recv(BUFFER_SIZE)
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
 
-
-        s.close()
     finally:
         s.close()
+    print("DONE")
 
 
 def one_of_each():
@@ -116,67 +146,114 @@ def one_of_each():
     s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
     s.connect((DESTINATION_IP, DESTINATION_PORT))
     print("Connected")
-    # Create Modbus packet
-    message = Modbus(READ_COILS_Q).raw
-    # Send
-    s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-    # Read response
-    data = s.recv(BUFFER_SIZE)
-    message = Modbus(READ_DISCRETE_INPUTS_Q).raw
-    s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-    # Read response
-    data = s.recv(BUFFER_SIZE)
-    message = Modbus(READ_HOLDING_REGISTERS_Q).raw
-    s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-    # Read response
-    data = s.recv(BUFFER_SIZE)
-    message = Modbus(WRITE_MULTIPLE_REGISTERS_Q).raw
-    s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-    # Read response
-    data = s.recv(BUFFER_SIZE)
-    message = Modbus(READ_INPUT_REGISTERS_Q).raw
-    s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-    # Read response
-    data = s.recv(BUFFER_SIZE)
-    message = Modbus(WRITE_SINGLE_COIL_Q).raw
-    s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
-    # Read response
-    s.close()
+    try:
+        message = Modbus(READ_COILS_Q).raw
+        s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
+        tic = datetime.now()
+        s.recv(BUFFER_SIZE)
+        toc = datetime.now()
+        w8(tic, toc)
+
+        message = Modbus(READ_DISCRETE_INPUTS_Q).raw
+        s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
+        tic = datetime.now()
+        s.recv(BUFFER_SIZE)
+        toc = datetime.now()
+        w8(tic, toc)
+
+        message = Modbus(READ_HOLDING_REGISTERS_Q).raw
+        s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
+        tic = datetime.now()
+        s.recv(BUFFER_SIZE)
+        toc = datetime.now()
+        w8(tic, toc)
+
+        message = Modbus(WRITE_MULTIPLE_REGISTERS_Q).raw
+        s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
+        tic = datetime.now()
+        s.recv(BUFFER_SIZE)
+        toc = datetime.now()
+        w8(tic, toc)
+
+        message = Modbus(READ_INPUT_REGISTERS_Q).raw
+        s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
+        tic = datetime.now()
+        s.recv(BUFFER_SIZE)
+        toc = datetime.now()
+        w8(tic, toc)
+
+        message = Modbus(WRITE_SINGLE_COIL_Q).raw
+        s.sendto(message, (DESTINATION_IP, DESTINATION_PORT))
+        s.recv(BUFFER_SIZE)
+
+    finally:
+        s.close()
+    print("DONE")
 
 
+def w8(tic, toc):
+    tdelta = (toc - tic).total_seconds()
+    if tdelta < DELAY:
+        time.sleep(DELAY-tdelta)
 
 
-
+def interleaved(cod_1, cod_2):
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((DESTINATION_IP, DESTINATION_PORT))
+    message1 = Modbus(code(cod_1)).raw
+    message2 = Modbus(code(cod_2)).raw
+    print("Connected")
+    try:
+        for i in range(N_PACKETS):
+            s.sendto(message1, (DESTINATION_IP, DESTINATION_PORT))
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
+            s.sendto(message2, (DESTINATION_IP, DESTINATION_PORT))
+            tic = datetime.now()
+            s.recv(BUFFER_SIZE)
+            toc = datetime.now()
+            w8(tic, toc)
+    finally:
+        s.close()
+    print("DONE")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="Generate a ModbusTCP traffic to specified host addr:port ")
     parser.add_argument("addr", help="Slave IP address", type=str)
     parser.add_argument("port", type=int, help="Slave TCP port", default=502)
-    parser.add_argument("--option", help="Specifies the burst option: ", type=int, choices=[1, 2, 3], default=3)
-    parser.add_argument("--code", help="Specifies Modbus function code in decimal of the burst: ", type=int, choices=[1, 2, 3, 4, 15])
-    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true")
+    parser.add_argument("--option", help="Specifies the burst option: ", type=int, choices=[1, 2, 3, 4], default=3)
+    parser.add_argument("-d", "--delay", help="Specifies the delay between sended packets: ", type=float, default=0.15)
+    parser.add_argument("-n", "--number", help="Specifies the number of packets per burst: ", type=int, default=1000)
+    parser.add_argument("--code", help="Specifies Modbus function code in decimal of the burst: ", type=int,
+                        choices=[1, 2, 3, 4, 15], action="append")
+    parser.add_argument("-v", "--verbose", help="increase output verbosity", action="store_true", default=False)
 
     args = parser.parse_args()
-    if args.option==1 and args.code is None:
+    if args.option == 1 and args.code is None:
         parser.error("--option requires --code")
+    if args.option == 4 and len(args.code) < 2:
+        parser.error("--option 4 requires --code [code_1] [code_2]")
 
     DESTINATION_IP = args.addr
     DESTINATION_PORT = args.port
-    op = args.option
-    code_value = args.code
+    N_PACKETS = args.number
+    DELAY = args.delay
+    VERB = args.verbose
 
-    # if args.verbosity:
-
-    if op is 1:
+    if args.option is 1:
         print("Starting continuous traffic...")
-        continuous(code_value)
-    elif op is 2:
+        continuous(args.code[0])
+    elif args.option is 2:
         print("Starting bursts of Modbus packets...")
-        #bursts_of_main()
-    elif op is 3:
+        bursts_of_main()
+    elif args.option is 3:
         print("Sending one packet of each Modbus function code...")
-        #one_of_each()
+        one_of_each()
+    elif args.option is 4:
+        print("Starting Interleaved burst with function codes {} and {}".format(args.code[0], args.code[1]))
+        interleaved(cod_1=args.code[0], cod_2=args.code[1])
     else:
-        print("Invalid option.")
-        exit(2)
+        raise Exception("OptionError: Invalid option.")
